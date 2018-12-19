e03438a7a9d1b33e1b6e246126a7b7bf87285057
Andrey <andrey.soktoev@gmail.com>
2018-12-20 05:20:21 +0900

    Create map

--- Choropleth Map/index.js
+++ Choropleth Map/index.js
@@ -1,16 +1,31 @@
 const
-  w = 800,
-  h = 600;
+  w = 960,
+  h = 640,
+  path = d3.geoPath();
 
 const svg = d3
-  .select('#map')
+  .select('body')
   .append('svg')
   .attr('width', w)
   .attr('height', h);
 
-const projection = geoEqualEarth();
+d3.json('counties.json').then((us) => {
 
-svg
-  .append('path')
-  .datum('https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json')
-  .attr('d', geoPath(projection));
+  svg
+    .append('g')
+    .selectAll('path')
+    .data(topojson.feature(us, us.objects.counties).features)
+    .enter()
+    .append('path')
+    .attr('fill', 'lightsalmon')
+    .attr('d', path);
+
+  svg
+    .append('path')
+    .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
+    .attr('fill', 'none')
+    .attr('stroke', 'white')
+    .attr('stroke-linejoin', 'round')
+    .attr('d', path);
+
+});
