e5462a688b6af24155eea4612670f0e1af9c4216
Andrey <andrey.soktoev@gmail.com>
2018-12-22 05:16:52 +0900

    Add gradients

--- Choropleth Map/index.js
+++ Choropleth Map/index.js
@@ -3,6 +3,7 @@ const
   e = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json',
   w = 960,
   h = 640,
+  k = 9,
   path = d3.geoPath();
 
 const svg = d3
@@ -20,6 +21,17 @@ Promise.all([u, e].map(i => d3.json(i))).then((results) => {
 
   [us, edu] = results;
 
+  const
+    min = d3.min(edu, d => d.bachelorsOrHigher),
+    max = d3.max(edu, d => d.bachelorsOrHigher),
+    r = d3.range(min, max, (max - min) / k),
+    someScale = d3
+      .scaleQuantize()
+      .domain([min, max])
+      .range(["#29ABE2", "#2D98DD", "#3185D9", "#3572D5", "#395FD1", "#3E4CCC", "#4239C8", "#4626C4", "#4A13C0", "#4F00BC"]);
+
+  console.log(d3.interpolateBlues(.5));
+
   svg
     .append('g')
     .selectAll('path')
@@ -28,12 +40,16 @@ Promise.all([u, e].map(i => d3.json(i))).then((results) => {
     .append('path')
     .attr('class', 'county')
     .attr('d', path)
-    .on('mouseover', (d) => tooltip
+    .attr('data-fips', d => d.id)
+    .attr('data-education', d => edu.filter(i => i.fips == d.id)[0].bachelorsOrHigher)
+    .style('fill', d => someScale(
+      edu.filter(i => i.fips == d.id)[0].bachelorsOrHigher
+    ))
+    .on('mouseover', d => tooltip
       .style('display', 'inline')
-      .attr('data-education', d.id)
-      .html(edu.length)
+      .html('Test')
     )
-    .on('mouseout', (d) => tooltip
+    .on('mouseout', d => tooltip
       .style('display', 'none')
       .attr('data-education', null)
       .html(null)
