39c14fe48168c6fa9f7d9afc17aa652d8add8200
Andrey <andrey.soktoev@gmail.com>
2018-12-22 21:24:37 +0900

    Legend

--- Choropleth Map/index.js
+++ Choropleth Map/index.js
@@ -2,8 +2,10 @@ const
   u = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json',
   e = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json',
   w = 960,
-  h = 640,
-  k = 9,
+  h = 620,
+  lrw = 50,
+  lrh = 10,
+  c = ['#29ABE2', '#2F8EDB', '#3572D5', '#3C55CF', '#4239C8', '#481CC2', '#4F00BC'],
   path = d3.geoPath();
 
 const svg = d3
@@ -11,6 +13,38 @@ const svg = d3
   .append('svg')
   .attr('width', w)
   .attr('height', h);
+
+const legend = d3
+  .select('body')
+  .append('svg')
+  .attr('width', lrw * c.length)
+  .attr('height', lrh * 3)
+  .attr('id', 'legend');
+
+legend
+  .selectAll('rect')
+  .data(c)
+  .enter()
+  .append('rect')
+  .attr('width', lrw)
+  .attr('height', lrh)
+  .attr('x', (d, i) => i * lrw)
+  .attr('y', 0)
+  .style('fill', d => d);
+
+const
+  lScale = d3
+    .scaleLinear()
+    .domain([0, 70])
+    .rangeRound([0, lrw * c.length]),
+  lAxis = d3.axisBottom(lScale).tickValues([10, 20, 30, 40, 50, 60]).tickSize(0);
+
+legend
+  .append('g')
+  .attr('transform', 'translate(0, ' + (lrh + 5) + ')')
+  .attr('class', 'axis')
+  .call(lAxis)
+  .call(g => g.select('.domain').remove());
 
 const tooltip = d3
   .select('body')
@@ -21,16 +55,11 @@ Promise.all([u, e].map(i => d3.json(i))).then((results) => {
 
   [us, edu] = results;
 
-  const
-    min = d3.min(edu, d => d.bachelorsOrHigher),
-    max = d3.max(edu, d => d.bachelorsOrHigher),
-    r = d3.range(min, max, (max - min) / k),
-    colors = d3
-      .scaleQuantize()
-      .domain([0, 60])
-      .range(["#29ABE2", "#3088DA", "#3866D2", "#3F44CB", "#4722C3", "#4F00BC"]);
+  const colors = d3
+    .scaleQuantize()
+    .domain([0, 60])
+    .range(c);
 
-  console.log(max);
   svg
     .append('g')
     .selectAll('path')
@@ -38,6 +67,9 @@ Promise.all([u, e].map(i => d3.json(i))).then((results) => {
     .enter()
     .append('path')
     .attr('class', 'county')
+    .attr('stroke', '#320078')
+    .attr('stroke-width', '.1')
+    .attr('stroke-linejoin', 'round')
     .attr('d', path)
     .attr('data-fips', d => d.id)
     .attr('data-education', d => edu.filter(i => i.fips == d.id)[0].bachelorsOrHigher)
